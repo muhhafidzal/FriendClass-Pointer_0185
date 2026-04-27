0eb84f48ed6e3bebf05599125d40d62853253f47
muhhafidzal <muh.hafidz.al.mubaroq@gmai.com>
2026-04-27 18:11:07 +0700

    Menambahkan class BelahKetupat dengan method kelilingFriend

--- tugasFriendClassPointer.cpp
+++ tugasFriendClassPointer.cpp
@@ -17,3 +17,18 @@ class LayangLayang
         // Friend function declaration
         friend class BelahKetupat;
 };
+
+class BelahKetupat
+{
+    private:
+        float d1, d2;
+        float sisi;
+    public:
+        void input();
+        float Luas();
+        float Keliling();
+        void output();
+        
+        // Friend function untuk menghitung keliling dari LayangLayang
+        float kelilingFriend(LayangLayang L);
+};
