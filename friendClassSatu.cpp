79ecd8078bfe53d719e2f6e8f9a2ba4f7cab6ada
muhhafidzal <muh.hafidz.al.mubaroq@gmai.com>
2026-04-27 15:14:17 +0700

    Melengkapi implementasi class orang dan siswa serta fungsi displayAll

--- friendClassSatu.cpp
+++ friendClassSatu.cpp
@@ -20,5 +20,27 @@ private:
 
 public:
     void setId(int pId);
-    void displayALL(orang &a);
+    void displayAll(orang &a);
 };
+
+void siswa::displayAll(orang &a)
+{
+    cout << id << endl << a.nama;
+}
+void orang::setNama(string pNama)
+{
+    nama = pNama;
+}
+void siswa::setId(int pId)
+{
+    id = pId;
+}
+int main()
+{
+    orang joko;
+    joko.setNama("Joko Kumat");
+    siswa joko_siswa;
+    joko_siswa.setId(1);
+    joko_siswa.displayAll(joko);
+    return 0;
+}
